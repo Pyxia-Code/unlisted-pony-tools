dd737530e3ecd202e9f4188a5977d24da7392fc0
Pyxia-Code <Pyxia@protonmail.ch>
2021-07-22 15:17:37 +0200

    Add support of /embed/ID links in recursive_file_scanner.py

--- recursive_file_scanner.py
+++ recursive_file_scanner.py
@@ -25,6 +25,8 @@ def main():
 				vids.add(vid)
 			for vid in re.findall("/v/([a-zA-Z0-9_-]{11})", data):
 				vids.add(vid)
+			for vid in re.findall("/embed/([a-zA-Z0-9_-]{11})", data):
+				vids.add(vid)
 			for vid in re.findall("youtu\.be/([a-zA-Z0-9_-]{11})", data):
 				vids.add(vid)
 			for playlist in re.findall("[&?]list=([a-zA-Z0-9_-]{18,34})", data):
